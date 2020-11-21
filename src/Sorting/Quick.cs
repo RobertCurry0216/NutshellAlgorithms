affac21a1439c881dafa88610c4765fb0f089b1b
Robert <robertcurry0216@gmail.com>
2020-11-22 08:22:43 +1100

    Class and test files set up

---
+++ src/Sorting/Quick.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nutshell.Sorting
+{
+    public static class Quick
+    {   
+        public static IEnumerable<int> Sort(IEnumerable<int> values)
+        {
+            return values;
+        }
+    }
+}
