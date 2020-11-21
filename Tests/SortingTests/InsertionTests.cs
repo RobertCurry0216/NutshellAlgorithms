cf6e95c3c2dd23da3b8f6f99b79c1b133a468b1c
Robert <robertcurry0216@gmail.com>
2020-11-21 11:06:14 +1100

    simplified tests

--- Tests/SortingTests/InsertionTests.cs
+++ Tests/SortingTests/InsertionTests.cs
@@ -13,18 +13,19 @@ namespace Tests.SortingTests
         {
         }
 
-        [TestCase(new[] {1,2,3,4,5}, new[] { 1, 2, 3, 4, 5 })]
-        [TestCase(new[] {5,4,3,2,1}, new[] { 1, 2, 3, 4, 5 })]
-        [TestCase(new[] {4,2,5,3,1}, new[] { 1, 2, 3, 4, 5 })]
-        [TestCase(new[] {1,1,1,1,1}, new[] { 1, 1, 1, 1, 1 })]
-        public void InsertionSort_Test(IEnumerable<int> mixed, IEnumerable<int> ordered)
+        [TestCase(new[] {1,2,3,4,5})]
+        [TestCase(new[] {5,4,3,2,1})]
+        [TestCase(new[] {4,2,5,3,1})]
+        [TestCase(new[] {1,1,1,1,1})]
+        public void InsertionSort_Test(IEnumerable<int> mixed)
         {
             var sorted = Nutshell.Sorting.Insertion.Sort(mixed).ToList();
-            var exp = ordered.ToList();
+            var prev = sorted.First();
 
             for (int i = 0; i < sorted.Count; i++)
             {
-                Assert.AreEqual(sorted[i], exp[i]);
+                Assert.IsTrue(sorted[i] >= prev);
+                prev = sorted[i];
             }
         }
     }
