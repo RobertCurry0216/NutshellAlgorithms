6abdfded89c3c600231203e09b9eb61c3feea0fc
Robert <robertcurry0216@gmail.com>
2020-11-28 09:03:40 +1100

    Files set up

---
+++ src/Sorting/Merge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nutshell.Sorting
+{
+    public static class Merge
+    {
+        public static IEnumerable<int> Sort (IEnumerable<int> values)
+        {
+            return values;
+        }
+    }
+}
