273262329b423677ec3ce2dee5a682fe48bc629b
Robert <robertcurry0216@gmail.com>
2021-01-03 13:59:04 +1100

    Sequential Search working

---
+++ Tests/SearchingTests/SequentialTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Tests.SearchingTests
+{
+    class SequentialTests
+    {
+        List<int> Values = new List<int>();
+
+        [SetUp]
+        public void Setup()
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Values.Add(rnd.Next(1000));
+            }
+
+            Values.Add(1001);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, true)]
+        [TestCase(new[] { 2, 3, 4, 5, 1 }, true)]
+        [TestCase(new[] { 0, 2, 3, 4, 5 }, false)]
+        public void SequentialSearch_Test(IEnumerable<int> values, bool exists)
+        {
+            var found = Nutshell.Searching.Sequential.Exists(values, 1);
+            Assert.AreEqual(found, exists);
+        }
+
+        [Test]
+        public void SequentialSearch_Big_Test()
+        {
+            var found = Nutshell.Searching.Sequential.Exists(Values, 1);
+            Assert.IsTrue(found);
+        }
+    }
+}
---
+++ src/Searching/Sequential.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nutshell.Searching
+{
+    public static class Sequential
+    {
+        public static bool Exists(IEnumerable<int> values, int value)
+        {
+            foreach (var v in values)
+            {
+                if (v == value) return true;
+            }
+            return false;
+        }
+    }
+}
